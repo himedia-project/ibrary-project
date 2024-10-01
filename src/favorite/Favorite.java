28e6542b4bf1850362f4a2cfdbf2f2c062eff525
seonggon do <mooh2jj@naver.com>
2024-10-01 16:32:45 +0900

    favorites 도메인 메서드 클래스 이름 정리

--- src/favorite/Favorite.java
+++ src/favorite/Favorite.java
@@ -1,24 +1,19 @@
 package favorite;
 
-//내가 입력한 책의 정보를 저장함
 
 public class Favorite {
 	
 	private Long id;  //입력받을 문자열 형식 객체
-	private String user_id;
-	private String book_id;
-	
+	private String userId;
+	private String bookId;
 	
 
-	public Favorite(Long id, String puserId, String pbookId) {
+	public Favorite(Long id, String userId, String bookId) {
 		this.id=id;
-		this.user_id=puserId;
-		this.book_id=pbookId;
-	} // 생성자선언 , (매개변수) 
+		this.userId =userId;
+		this.bookId =bookId;
+	}
 
-
-
-	//읽기접근, 쓰기 접근 설정
 	public Long getId() {
 		return id;
 	}
@@ -27,22 +22,29 @@ public class Favorite {
 		this.id = id;
 	}
 
-	public String getuserId() {
-		return user_id;
+	public String getUserId() {
+		return userId;
 	}
 
-	public void setuserId(String userid) {
-		this.user_id = user_id;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
-	public String getbookId() {
-		return book_id;
+	public String getBookId() {
+		return bookId;
 	}
 
-	public void setbookId(String bookid) {
-		this.book_id = book_id;
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
 	}
 
-
+	@Override
+	public String toString() {
+		return "Favorite{" +
+				"id=" + id +
+				", userId='" + userId + '\'' +
+				", bookId='" + bookId + '\'' +
+				'}';
+	}
 }
 
