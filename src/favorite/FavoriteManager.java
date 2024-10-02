eeb99ae1674c1df984fe7f6b494f82452760468f
seonggon do <mooh2jj@naver.com>
2024-10-02 11:50:12 +0900

    Book Repository 내 즐겨찾기 용 북리스트 메서드 추가

--- src/favorite/FavoriteManager.java
+++ src/favorite/FavoriteManager.java
@@ -1,14 +1,19 @@
 package favorite;
 
 
+import book.Book;
+import book.BookRepository;
+
 import java.util.List;
 
 public class FavoriteManager {
 
     private FavoriteRepository favoriteRepository = null;
+    private BookRepository bookRepository = null;
 
     public FavoriteManager() {
         favoriteRepository = new FavoriteRepository();
+        bookRepository = new BookRepository();
     }
 
 
@@ -40,10 +45,15 @@ public class FavoriteManager {
      * @param userId 사용자 아이디
      */
     public void viewCart(String userId) {
-        // TODO: 즐겨찾기 북리스트 보기
-        List<Favorite> favorites = favoriteRepository.allFetch(userId);
-        for (Favorite favorite : favorites) {
-            System.out.println(favorite);
+
+        List<String> bookIdList = favoriteRepository.findFavoriteBookIdListByUserId(userId);
+
+        List<Book> bookList = bookRepository.findBookListByIdList(bookIdList);
+        for (Book book : bookList) {
+            System.out.printf("ISBN: %s, 타이틀: %s, 지은이: %s, 출판사: %s, 카테고리: %s",
+                    book.getId(), book.getTitle(),
+                    book.getTitle(), book.getWriter(), book.getCategory());
+            System.out.println();
         }
     }
 }
